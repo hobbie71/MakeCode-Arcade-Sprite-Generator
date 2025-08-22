8163479322235fa2b1c5c15b9a67bc0c5f57827c
Javier Tamayo <javierj.tamayo@gmail.com>
2025-08-22 13:22:03 -0400

    Fixed ImageToSpriteSection

---
+++ client/src/features/InputSection/GenerationMethodSection/ImageToSpriteSection/ImageToSpriteSection.tsx
@@ -0,0 +1,12 @@
+// Component imports
+import ImageUploadForm from "./components/ImageUploadForm";
+
+const ImageToSpriteSection = () => {
+  return (
+    <div className="">
+      <ImageUploadForm />
+    </div>
+  );
+};
+
+export default ImageToSpriteSection;
