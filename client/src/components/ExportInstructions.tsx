379b23b9c3b8d0173e5ea954f5994ad58895f51d
Javier Tamayo <javierj.tamayo@gmail.com>
2025-10-20 09:59:17 -0400

    Updated export instructions

--- client/src/components/ExportInstructions.tsx
+++ client/src/components/ExportInstructions.tsx
@@ -1,8 +1,11 @@
 import X_Button from "./X_Button";
+import { OS } from "../utils/getOS";
 
 interface Props {
   closeExportInstructions: () => void;
 }
+
+const command = OS === "mac" ? "Cmd" : "Ctrl";
 
 const ExportInstructions = ({ closeExportInstructions }: Props) => {
   return (
@@ -16,26 +19,28 @@ const ExportInstructions = ({ closeExportInstructions }: Props) => {
       </h4>
 
       <div className="mb-4 flex flex-col gap-6">
-        <ol className="list-decimal list-inside text-neutral-300 text-sm text-left space-y-2">
+        <ol className="list-decimal list-inside text-text-default-200 text-sm text-left space-y-2">
           <li>
-            Copy the <b>Sprite Editor Code</b> in <b>Export Sprite</b> section.
+            Copy the <b className="text-text-default-300">Sprite Editor Code</b>{" "}
+            in <b className="text-text-default-300">Export Sprite</b> section.
           </li>
           <li>
-            Open the <b>MakeCode Arcade Sprite Editor</b>.
+            Open the{" "}
+            <b className="text-text-default-300">
+              MakeCode Arcade Sprite Editor
+            </b>
+            .
           </li>
           <li>Click inside the Sprite Editor canvas</li>
           <li>
-            Paste the code directly into the editor using:
-            <div>
-              Windows: <span className="font-mono">Ctrl + V</span>
-            </div>
-            <div>
-              Apple: <span className="font-mono">Command + V</span>
-            </div>
+            Paste sprite into the editor using:{" "}
+            <span className="font-mono font-bold text-text-default-300">
+              {command} + V
+            </span>
           </li>
           <li>Your sprite will appear in the editor, ready to use!</li>
         </ol>
-        <div className="text-neutral-300 text-sm">
+        <div className="text-text-default-200 text-sm">
           Need help? See the animation below for a visual guide.
         </div>
         <img
