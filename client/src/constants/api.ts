f39603e9c574e03d1bf10495c4db612ba37d6e96
Javier Tamayo <javierj.tamayo@gmail.com>
2025-12-20 20:11:14 -0500

    Updated base localhost port

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,6 +1,4 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  (import.meta.env.PROD ? "" : "http://localhost:8000");
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:62274";
 
 export const GENERATE_PIXELLAB_IMAGE_API_URL = `${API_BASE_URL}/generate-image/pixellab`;
 export const GENERATE_OPENAI_IMAGE_API_URL = `${API_BASE_URL}/generate-image/openai`;
